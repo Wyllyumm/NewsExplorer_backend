b5f02f7757ae4388b223f2fe085e9a34e5d35cc8
Wyllyumm <walmozard@hotmail.com>
2025-07-07 10:11:11 -0400

    added newsArticles route

--- routes/newsArticles.js
+++ routes/newsArticles.js
@@ -1,1 +1,14 @@
+const router = require("express").Router();
 
+const {
+  getItems,
+  likeItem,
+  dislikeItem,
+} = require("../controllers/newsArticles.js");
+const { validateItemId } = require("../middlewares/validation.js");
+
+router.get("/", getItems);
+router.put("/:itemId/likes", validateItemId, likeItem);
+router.delete("/:itemId/likes", validateItemId, dislikeItem);
+
+module.exports = router;
